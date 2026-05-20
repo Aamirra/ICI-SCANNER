929f585966ae024bbf58430cc61214c741715dcb
Aamirra <81755807+Aamirra@users.noreply.github.com>
2026-05-21 00:21:11 +0500

    Create broadcast.js

---
+++ services/broadcast.js
@@ -0,0 +1,30 @@
+const https = require('https');
+const admin = require('firebase-admin');
+const config = require('../config');
+
+let lastBroadcastTimestamp = 0;
+
+async function checkBroadcasts() {
+    const url = `${config.FIREBASE_URL}/broadcast.json`;
+    https.get(url, (res) => {
+        let d = '';
+        res.on('data', chunk => d += chunk);
+        res.on('end', () => {
+            try {
+                const data = JSON.parse(d);
+                if (data && data.timestamp > lastBroadcastTimestamp) {
+                    if (lastBroadcastTimestamp !== 0) {
+                        const message = {
+                            notification: { title: "ICI Update", body: data.message },
+                            topic: 'all_users'
+                        };
+                        admin.messaging().send(message).catch(() => {});
+                    }
+                    lastBroadcastTimestamp = data.timestamp;
+                }
+            } catch (e) {}
+        });
+    }).on('error', () => {});
+}
+
+module.exports = checkBroadcasts;
