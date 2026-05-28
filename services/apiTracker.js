179b5278fc39b413efa34ecd1eee3861b9e1fc33
Aamirra <81755807+Aamirra@users.noreply.github.com>
2026-05-28 18:08:38 +0500

    Update apiTracker.js

--- services/apiTracker.js
+++ services/apiTracker.js
@@ -1,6 +1,31 @@
+const https = require('https');
+const config = require('../config');
 const firebasePut = require('./database');
 
-function updateApiStatus(keyUsage) {
+let keyUsage = {};
+
+// Firebase se pehle saved remaining lo
+async function loadApiStatus() {
+    return new Promise((resolve) => {
+        const url = `${config.FIREBASE_URL}/api_status.json`;
+        https.get(url, (res) => {
+            let d = '';
+            res.on('data', chunk => d += chunk);
+            res.on('end', () => {
+                try {
+                    const data = JSON.parse(d);
+                    if (data && data.remaining) {
+                        console.log(`API status loaded: ${data.remaining}/${data.total}`);
+                    }
+                } catch(e) {}
+                resolve();
+            });
+        }).on('error', () => resolve());
+    });
+}
+
+function updateApiStatus(usage) {
+    keyUsage = usage;
     const totalRemaining = Object.values(keyUsage).reduce((a, b) => a + b, 0);
     const totalKeys = Object.keys(keyUsage).length;
     const totalLimit = totalKeys * 800;
@@ -11,4 +36,4 @@ function updateApiStatus(keyUsage) {
     });
 }
 
-module.exports = updateApiStatus;
+module.exports = { updateApiStatus, loadApiStatus };
