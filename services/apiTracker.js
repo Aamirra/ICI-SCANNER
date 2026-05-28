61ba8a9e500c33bfb0810adb4cb622e4a9f89bef
Aamirra <81755807+Aamirra@users.noreply.github.com>
2026-05-28 18:07:39 +0500

    Update apiTracker.js

--- services/apiTracker.js
+++ services/apiTracker.js
@@ -2,9 +2,11 @@ const firebasePut = require('./database');
 
 function updateApiStatus(keyUsage) {
     const totalRemaining = Object.values(keyUsage).reduce((a, b) => a + b, 0);
+    const totalKeys = Object.keys(keyUsage).length;
+    const totalLimit = totalKeys * 800;
     firebasePut('api_status', {
         remaining: totalRemaining,
-        total: 12800,
+        total: totalLimit,
         timestamp: Date.now()
     });
 }
