cf111e52628cb77ac3e996b15dec571a75ab43b8
Aamirra <81755807+Aamirra@users.noreply.github.com>
2026-05-28 17:53:51 +0500

    Update apiTracker.js

--- services/apiTracker.js
+++ services/apiTracker.js
@@ -1,39 +1,12 @@
-const https = require('https');
-const config = require('../config');
 const firebasePut = require('./database');
 
-let keyUsage = {};
-
-// Firebase se pehle saved remaining lo
-async function loadApiStatus() {
-    return new Promise((resolve) => {
-        const url = `${config.FIREBASE_URL}/api_status.json`;
-        https.get(url, (res) => {
-            let d = '';
-            res.on('data', chunk => d += chunk);
-            res.on('end', () => {
-                try {
-                    const data = JSON.parse(d);
-                    if (data && data.remaining) {
-                        console.log(`API status loaded: ${data.remaining}/${data.total}`);
-                    }
-                } catch(e) {}
-                resolve();
-            });
-        }).on('error', () => resolve());
-    });
-}
-
-function updateApiStatus(usage) {
-    keyUsage = usage;
+function updateApiStatus(keyUsage) {
     const totalRemaining = Object.values(keyUsage).reduce((a, b) => a + b, 0);
-    const totalKeys = Object.keys(keyUsage).length;
-    const totalLimit = totalKeys * 800;
     firebasePut('api_status', {
         remaining: totalRemaining,
-        total: totalLimit,
+        total: 12800,
         timestamp: Date.now()
     });
 }
 
-module.exports = { updateApiStatus, loadApiStatus };
+module.exports = updateApiStatus;
