314c4e5e8f1c1af20d8842742032c518fbf1af6e
Aamirra <81755807+Aamirra@users.noreply.github.com>
2026-06-10 19:27:13 +0500

    Create StockList.js

---
+++ StockList.js
@@ -0,0 +1,19 @@
+module.exports = [
+    "AAPL", "MSFT", "NVDA", "GOOGL", "AMZN", "META", "TSLA", "NFLX",
+    "AVGO", "AMD", "INTC", "QCOM", "CSCO", "ORCL", "IBM", "CRM",
+    "ADBE", "ASML", "TSM",
+    "JPM", "BAC", "WFC", "C", "GS", "MS", "V", "MA", "PYPL", "AXP",
+    "BRK/B",
+    "WMT", "HD", "COST", "TGT", "NKE", "SBUX", "MCD", "KO", "PEP",
+    "PG", "EL", "BABA", "JD", "PDD",
+    "DIS", "CMCSA", "T", "VZ", "TMUS", "SONY", "SPOT",
+    "F", "GM", "TM", "CAT", "GE", "BA", "MMM", "LMT",
+    "JNJ", "PFE", "MRK", "ABV", "LLY", "UNH", "MRNA", "BMY", "GILD",
+    "XOM", "CVX", "SHEL", "BP", "COP",
+    "ABNB", "UBER", "LYFT", "SNAP", "PINS", "TWLO", "ZM", "DOCU",
+    "PLTR", "COIN", "HOOD", "SQ", "SHOP", "SE", "MELI",
+    "BIDU", "NIO", "LI", "XPEV", "FUTU", "XERC", "HPQ", "DELL",
+    "WDC", "STX", "MU", "TXN", "ADI", "NXPI", "MCHP", "ON",
+    "PANW", "FTNT", "CRWD", "NET", "DDOG", "SNOW", "TEAM", "WDAY",
+    "NOW", "SNPS", "CDNS"
+];
