279ac48d8337390bcfc9c2bc6969e24fbadc0e9d
Aamirra <81755807+Aamirra@users.noreply.github.com>
2026-06-11 09:10:21 +0500

    Create psxStocksList.js

---
+++ psxStocksList.js
@@ -0,0 +1,11 @@
+module.exports = [
+    "SYS", "TRG", "AIRLINK", "PTC", "TELE", "NETSOL", "WTL",
+    "MEBL", "UBL", "MCB", "HBL", "NBP", "BOP", "FABL", "BAHL",
+    "OGDC", "PPL", "MARI", "POL", "PSO", "APL", "SNGP", "SSGC",
+    "HUBC", "KEL", "KAPCO", "NCPL", "NPL",
+    "LUCK", "DGKC", "MLCF", "FCCL", "CHCC", "BWCL", "KOHC",
+    "FFC", "ENGRO", "EFERT", "FATIMA", "LCI", "LOTCHEM", "EPCL",
+    "INDU", "HCAR", "PSMC", "ATLH", "ISL", "INIL", "AGHA",
+    "ATRL", "PRL", "CYAN", "ILP", "NML", "KTML", "GADT",
+    "NESTLE", "FFL", "MUREB", "ABOT", "AGP", "HALEON", "CHAL"
+];
